094a6cbcdb2cc822e2bc09bc7edd604390831764
Fernando Olivero <fernando.olivero@ecloudsolutions.com>
2018-04-26 12:04:46 -0300

    FIX

--- services/managers/cluster-manager.js
+++ services/managers/cluster-manager.js
@@ -45,7 +45,7 @@ var ClusterManager = (function (_super) {
                     dv = Math.round(dv * 100) / 100;
                     const padding_left = markers.length > 9 ? 25 : 32;
                     return {
-                        text: "<small style='color:#f96363;font-size:11px;text-align:center'>\xa0\ <b>u$s"+ dv +"m²</b> </small>\x0D\ <span style='padding-left:25px;padding-top:2px;font-size:22px'>"+ markers.length +"</span> <small style='font-size:14px;padding-left:6px'>Proyectos</small>",
+                        text: "<small style='color:#f96363;font-size:11px;text-align:center'>\xa0\ <b>u$s"+ dv +"m²</b> </small>\x0D\ <span style='padding-left:"+padding_left+"px;padding-top:2px;font-size:22px'>"+ markers.length +"</span> <small style='font-size:14px;padding-left:6px'>Proyectos</small>",
                         index: index
                     }
                 }
